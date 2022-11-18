b45d4d9332b5cf6f62004d584accdfb054032980
azharanowar <azharanowar@gmail.com>
2022-11-18 12:14:05 +0600

    @extend and @section start and @endsection added

--- resources/views/frontEnd/include/footer.blade.php
+++ resources/views/frontEnd/include/footer.blade.php
@@ -123,19 +123,3 @@
     </div>
 
 </footer>
-
-<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-<!-- Vendor JS Files -->
-<script src="{{ asset('frontEndAssets') }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="{{ asset('frontEndAssets') }}/assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="{{ asset('frontEndAssets') }}/assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="{{ asset('frontEndAssets') }}/assets/vendor/aos/aos.js"></script>
-<script src="{{ asset('frontEndAssets') }}/assets/vendor/php-email-form/validate.js"></script>
-
-<!-- Template Main JS File -->
-<script src="{{ asset('frontEndAssets') }}/assets/js/main.js"></script>
-
-</body>
-
-</html>
